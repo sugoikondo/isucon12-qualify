dcfd519c4514ea0f454fc77f506cddcad809115d
atsumine kondo <sugoikondo@atsuminenoMacBook-Pro.local>
2023-11-18 10:35:22 +0900

    init spring boot

---
+++ webapp/kotlin/src/main/kotlin/isucon12/ConacApplication.kt
@@ -0,0 +1,11 @@
+package isucon12
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConacApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConacApplication>(*args)
+}
